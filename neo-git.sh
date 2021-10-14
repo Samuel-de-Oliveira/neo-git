eddb4926228d0b8495109dcf0d358a2eda65612f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-10-14 16:36:31 -0300

    Finish this part

--- neo-git.sh
+++ neo-git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-neo-push()
+push()
 {	git add .
 	git commit -m "$1"
 	git push
@@ -9,23 +9,24 @@ halp()
 {	echo -e "Command: nvim [--param...] ...\nThis is the list of parameters from neo-vim:\n"
 
 	echo -e "\t--push or -p >>: commit and push the repository in a command (nvim --push '(commit name...)');"
+	echo -e "\t--commit or -c >>: Just commit the repository in a command (nvim --commit '(commit name...)');"
 	echo -e "\t--help or -h >>: show the help message."
 }
 commit()
-{	echo "commit!"
+{	git add .
+	git commit -m "$1"
 }
 
 case $1 in 
 	
-"--push") neo-push "$2";;
-"-p") neo-push "$2";;
+"--push") push "$2";;
+"-p") push "$2";;
 
-"--commit") commit;;
-"-c") commit;;
+"--commit") commit "$2";;
+"-c") commit "$2";;
 
 "--help") halp;;
 "-h") halp;;
 
 *)echo -e "\nUps... Maybe you digit something wrong.\nDigit --help for help.";;
 esac
-
