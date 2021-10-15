582fbb7e31fecf9a9de6687f45c6efc9182ac799
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-10-15 13:47:35 -0300

    Realease

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # neo-git
-is like git, but upgraded.
+
+Neo-Git is a plugin for Git who make easier
+the commit and push. This program is easy 
+and fastest to use.
+
+To install you need to make the following
+procediments (On linux):
+	
+	git clone https://github.com/Samuel-de-Oliveira/neo-git
+	cd neo-git
+	sudo ./install.sh
+
+To use you just need make this `ngit [--command] ...` in terminal	
+
+#### to see command list enter `ngit -h` in your terminal
+*<strong>Created by:</strong> Samuel de Oliveira 2021*
--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/bash
 
 clear
+echo "Neo-git version 1.0"
 echo "Are you sure you want to install? [Y/n]"; read cmd
 clear
 
 if [ $cmd == 'y' ] || [ $cmd == 'Y' ]; then
 
 	echo -e "The program is installing!\n"
+	
+	if [ ! -f /usr/bin/git ]; then
+		echo "Installing git"
+		if [ -f /etc/debian_version ]; then
+			sudo apt install git -y
+		elif [ -f /etc/arch_version ]; then
+			sudo pacman -Syy git
+		fi
+	fi
 
 	echo "Moving the main file to /usr/bin/"
 	sudo cp neo-git.sh /usr/bin/ngit
 	
 	echo -e "\nThe installing is terminated!"
 	echo "Press return to exit..."; read
+
 	clear
 else
 	echo "abort!"	
