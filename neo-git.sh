acb8ae0148b81432c39f04caa3901a23c5728c9d
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-11-03 21:27:04 -0300

    Edit code

--- neo-git.sh
+++ neo-git.sh
@@ -14,6 +14,8 @@
 #						#
 #--*-----------------------------------------*--#
 
+
+# Here are all functions who is used in parameters.
 push()
 {	git add .
 	git commit -m "$1"
@@ -31,28 +33,26 @@ halp()
 	echo -e "\t \033[34;1m--help or -h \033[m>>: show the help message (nvim --help).\n"
 }
 version()
-{	echo -e "\nNeoGit version: 1.0\nhave no warranty.\n"
+{	echo -e "\nNeoGit version: \033[33;1m1.1\033[m\nhave no warranty.\n"
 }
 
+# If the user digit nothing, will show the help message.
 if [ $# -eq 0 ]; then
 	halp
 	exit 1
 fi
 
+# Parameters:
 case $1 in
 	
-"--push") push "$2";;
-"-p") push "$2";;
+"--push" | "-p") push "$2";;
 
-"--commit") commit "$2";;
-"-c") commit "$2";;
+"--commit" | "-c") commit "$2";;
 
-# I have used "halp" because the command "help" is reserved.
-"--help") halp;;
-"-h") halp;;
+"--help" | "-h") halp;; # I have created "halp" because the command "help" is reserved.
 
-"--version") version;;
-"-v") version;;
+"--version" | "-v") version;;
 
-*)echo -e "\n\033[31mUps... Maybe you digit something wrong.\033[m\nDigit --help for help.\n";;
+*) echo -e "\n\033[31mUps... Maybe you digit something wrong.\033[m\nDigit --help for help.\n";;
+
 esac
