9ec2affef40873b51f2fef0eb7002582261b365d
Samuel-de-Oliveira <samwolfg13@gmail.com>
2021-11-03 21:47:53 -0300

    test

--- neo-git.sh
+++ neo-git.sh
@@ -17,27 +17,42 @@
 
 # Here are all functions who is used in parameters.
 push()
-{	git add .
+{	if [ $# == 0 ]; then
+		echo -e "\n\033[31mMissing arguments...\033[m\nDigit \"ngit --help\" for help\n"
+		exit 1
+	fi
+	git add .
 	git commit -m "$1"
 	git push
 }
 commit()
-{	git add .
+{	if [ $# == 0 ]; then
+		echo -e "\n\033[31mMissing arguments...\033[m\nDigit \"ngit --help\" for help\n"
+		exit 1
+	fi
+	git add .
 	git commit -m "$1"
+}
+create()
+{	if [ $# == 0 ]; then
+		echo -e "\n\033[31mMissing arguments...\033[m\nDigit \"ngit --help\" for help\n"
+		exit 1
+	fi
+	echo "Hello!"
 }
 halp()
 {	echo -e "\nCommand: nvim [--parameter] ...\nThis is the list of parameters from neo-vim:\n"
 
-	echo -e "\t \033[34;1m--push or -p \033[m>>: commit and push the repository in a command (nvim --push '(commit name...)');"
+	echo -e "\t \033[34;1m--push or -p \033[m>>: Commit and push the repository in a command (nvim --push '(commit name...)');"
 	echo -e "\t \033[34;1m--commit or -c \033[m>>: Just commit the repository in a command (nvim --commit '(commit name...)');"
-	echo -e "\t \033[34;1m--help or -h \033[m>>: show the help message (nvim --help).\n"
+	echo -e "\t \033[34;1m--help or -h \033[m>>: Show the help message (nvim --help).\n"
 }
 version()
 {	echo -e "\nNeoGit version: \033[33;1m1.1\033[m\nhave no warranty.\n"
 }
 
 # If the user digit nothing, will show the help message.
-if [ $# -eq 0 ]; then
+if [ $# == 0 ]; then
 	halp
 	exit 1
 fi
@@ -53,6 +68,8 @@ case $1 in
 
 "--version" | "-v") version;;
 
-*) echo -e "\n\033[31mUps... Maybe you digit something wrong.\033[m\nDigit --help for help.\n";;
+"--create" | "-C") create;;
+
+*) echo -e "\n\033[31mUps... Maybe you digit something wrong.\033[m\nDigit \"ngit --help\" for help.\n";;
 
 esac
