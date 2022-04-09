8aed77ac4a315de3672a5372068e9eb129ace505
Samuel-de-Oliveira <samwolfg13@gmail.com>
2022-04-09 12:17:18 -0300

    Avoid errors

--- neo-git.sh
+++ neo-git.sh
@@ -30,11 +30,18 @@ repobuild()
 		echo -e "\n\033[31mLanguage not suported...\033[m\nDigit \"ngit --help\" for help\n"
 		exit 1
 	fi
+	if [ "$2" == "" ]; then
+		echo -e "\n\033[31mPlease digit a valid name for tamplate...\033[m\nDigit \"ngit --help\" for help\n"
+		exit 1
+	fi
 
 	# create folder and add files
 	mkdir $1
 	cd $1
-	echo "# $1" >> README.md
+	echo -e "# $1"                                               >> README.md
+	echo -e "This is the $1 repository, created by: [Your name]" >> README.md
+	echo -e "\n ## How it work\n[Your program info]"             >> README.md
+	echo -e "\n## Download\n[Download info]"                     >> README.md
 	cp /usr/lib/neo-git/langs/$2/* .
 
 	# Start git in repository
@@ -42,8 +49,6 @@ repobuild()
 	git add .
 	git commit -m "Ready..."
 	git branch -M master) &> /dev/null
-
-
 }
 filebuild()
 {	if [ $# -eq 0 ]; then
@@ -56,17 +61,17 @@ filebuild()
 	fi
 	if [ "$2" == "" ]; then
 		echo -e "\n\033[31mPlease digit a valid name for your file...\033[m\nDigit \"ngit --help\" for help\n"
+		exit 1
 	fi
 
 	cp /usr/lib/neo-git/langs/$1/main.* ./$2
-
 }
 halp()
 {	echo -e "\nCommand: ngit [--parameter] ...\nThis is the list of parameters from neo-vim:\n"
 
-	echo -e "\t \033[34;1m--create or -C \033[m>>: Create a versioned repository (nvim --create [repository's name] [language])"
+	echo -e "\t \033[34;1m--create or -C \033[m>>: Create a versioned repository (nvim --create [repository's name] [language template])"
 	echo -e "\t \033[34;1m--list or -l \033[m>>: Show the lis of suported languages"
-	echo -e "\t \033[34;1m--file or -f \033[m>>: Create a language template (nvim --file [language] [file name])"
+	echo -e "\t \033[34;1m--file or -f \033[m>>: Create a language template (nvim --file [language template] [file name])"
 	echo -e "\t \033[34;1m--version or -v \033[m>>: Show version (nvin --version)"
 	echo -e "\t \033[34;1m--help or -h \033[m>>: Show the help message (nvim --help).\n"
 }
