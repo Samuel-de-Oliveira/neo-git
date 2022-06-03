3f9cecf9ab5c2ebb4f57b5aa7d91fa156a621dc0
Samuel de Oliveira <81493939+Samuel-de-Oliveira@users.noreply.github.com>
2022-06-03 13:42:07 -0300

    Web templete library now create a new web folder

--- langs/web/maker.sh
+++ langs/web/maker.sh
@@ -3,7 +3,7 @@ DIRLOCALE=$(dirname -- $(readlink -fn -- "$0"))
 
 if [ $1 == "file" ]; then
 	if [ -f $NAME.html ]; then
-		echo "The file $NAME.html exist, please remove to proced"
+		echo "The file $NAME.html exist, please remove to proced..."
 		exit 1
 	fi
 
@@ -12,9 +12,14 @@ fi
 
 if [ $1 == "dir" ]; then
 	cp $DIRLOCALE/* .
-        rm -f maker.sh
+        rm -rf maker.sh
 fi
 
 if [ $1 == "lib" ]; then
-	echo -e "\nNo library for this language\n"
+        if [ -d $NAME ]; then
+          echo -e "The directory $NAME exist, please remove to proced..."
+	fi
+        mkdir -p $NAME
+        cp $DIRLOCALE/* ./$NAME
+	rm -rf maker.sh
 fi
